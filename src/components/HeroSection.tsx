a9ddac2053a04d25609c25dc24a43aa645464a9d
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2026-01-30 18:32:18 +0100

    small changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,12 +5,12 @@ import GhostSwarm from "./GhostSwarm";
 export default async function HeroSection({ lang }: { lang: Locale }) {
   const t = await getTranslations({ namespace: "Hero", locale: lang });
   return (
-    <section className="w-full py-1 md:py-24 lg:py-32 relative overflow-hidden flex flex-col items-center min-h-[75vh] justify-center">
+    <section className="w-full py-12 md:py-24 lg:py-32 relative overflow-hidden flex flex-col items-center min-h-[70vh] justify-center">
       <div className="container relative z-10">
         <div className="flex flex-row">
           <div className="flex flex-col justify-cente space-y-4">
             <div className="space-y-2">
-              <h1 className="text-5xl sm:text-6xl md:text-7xl xl:text-8xl font-extrabold text-neutral-800 uppercase">
+              <h1 className="text-6xl sm:text-6xl md:text-7xl xl:text-8xl font-extrabold text-neutral-800 uppercase">
                 <span>We build</span>
                 <br />
                 <span className="">Digital</span>
