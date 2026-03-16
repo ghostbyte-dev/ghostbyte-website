12249bf8f00f471ef41805aac2075b53ddc03486
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2026-03-16 18:30:47 +0100

    Add back to all projects button

--- src/components/projects/ProjectHero.tsx
+++ src/components/projects/ProjectHero.tsx
@@ -1,3 +1,4 @@
+import { ArrowLeftIcon } from "lucide-react";
 import Link from "next/link";
 
 interface ProjectStat {
@@ -15,15 +16,22 @@ const ProjectHero = ({ title, stats }: ProjectHeroProps) => {
   return (
     <section className="w-full">
       <div className="container mt-40 md:mt-52 mb-10">
+        <Link
+          href="/work"
+          className="flex items-center space-x-1.5 text-neutral-800"
+        >
+          <ArrowLeftIcon size={20} />
+          <span className="font-medium">All projects</span>
+        </Link>
         <div className="w-full">
-          <h1 className="font-extrabold text-neutral-800 text-4xl sm:text-4xl md:text-4xl lg:text-5xl mt-16 mb-3 leading-tight">
+          <h1 className="font-extrabold text-neutral-800 text-4xl sm:text-4xl md:text-4xl lg:text-5xl mt-10 mb-3 leading-tight">
             {title}
           </h1>
         </div>
 
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-x-3 gap-y-6  w-full mt-10">
-          {stats.map((stat, index) => (
-            <div key={index}>
+          {stats.map((stat) => (
+            <div key={stat.label}>
               <h2 className="text-xl font-semibold">{stat.label}</h2>
 
               {stat.href ? (
